2cf1fbd5aae45a630c25df42fcc0a416a9ea7d3e
David McKenna <mckennd2@tcd.ie>
2020-07-22 13:54:31 +0000

    Fifth time's the charm?

--- lofar_udp_reader.c
+++ lofar_udp_reader.c
@@ -2609,7 +2609,7 @@ int lofar_udp_raw_udp_stokesI(lofar_udp_meta *meta) {
 
 			}
 
-			outputPacketOffset = iLoop * packetOutputLength / (sizeof(float) / sizeof(signed short int));
+			outputPacketOffset = iLoop * packetOutputLength / (sizeof(float) / sizeof(char));
 
 			//#pragma omp parallel for schedule(dynamic, 31) // Expected sizes: 61, 122, 244
 			#pragma GCC unroll 61
